8083d90e2d3f424a3bd7659e598ad85d566e2024
SaBaLeRo22 <linkinpark.sabalero22@gmail.com>
2019-01-27 18:33:16 -0300

    Consultas

--- app/Controller/RespuestaSalariosController.php
+++ app/Controller/RespuestaSalariosController.php
@@ -23,7 +23,69 @@ class RespuestaSalariosController extends AppController {
  */
 	public function index() {
 		$this->RespuestaSalario->recursive = 0;
+		$this->paginate = array(
+			'limit' => '5',
+			'order' => array(
+				'Consulta.id' => 'asc', 'RespuestaSalario.id' => 'asc'
+			)
+		);
 		$this->set('respuestaSalarios', $this->Paginator->paginate());
+	}
+
+	public function csv()
+	{
+		$this->RespuestaSalario->recursive = 0;
+		$data = $this->RespuestaSalario->find('all', array(
+			'Consulta.id' => 'asc', 'RespuestaSalario.id' => 'asc'
+		));
+
+		foreach ($data as $key => $rp) {
+			if(!empty($rp['RespuestaSalario']['salario'])){$data[$key]['RespuestaSalario']['salario'] = number_format ( $rp['RespuestaSalario']['salario'], '2', ',', '.');}
+			if(!empty($rp['RespuestaSalario']['cantidad'])){$data[$key]['RespuestaSalario']['cantidad'] = number_format ( $rp['RespuestaSalario']['cantidad'], '2', ',', '.');}
+			if(!empty($rp['RespuestaSalario']['antiguedad'])){$data[$key]['RespuestaSalario']['antiguedad'] = number_format ( $rp['RespuestaSalario']['antiguedad'], '2', ',', '.');}
+			$data[$key]['RespuestaSalario']['estado'] = $rp['Estado']['nombre'];
+			$data[$key]['RespuestaSalario']['user_created'] = $this->Authake->getUsuario($rp['RespuestaSalario']['user_created']);
+			$data[$key]['RespuestaSalario']['user_modified'] = $this->Authake->getUsuario($rp['RespuestaSalario']['user_modified']);
+		}
+
+		$_delimiter = ';';
+		$_bom = true;
+		$_null = '';
+		$_serialize = 'data';
+
+		$_extract = array(
+			'RespuestaSalario.id',
+			'RespuestaSalario.consulta_id',
+			'RespuestaSalario.categoria',
+			'RespuestaSalario.salario',
+			'RespuestaSalario.cantidad',
+			'RespuestaSalario.antiguedad',
+			'RespuestaSalario.estado',
+			'RespuestaSalario.created',
+			'RespuestaSalario.modified',
+			'RespuestaSalario.user_created',
+			'RespuestaSalario.user_modified'
+		);
+
+		$excludePaths = array(); // Exclude all id fields
+		//$_extract = $this->CsvView->prepareExtractFromFindResults($data, $excludePaths);
+
+		$customHeaders = array();
+		$options = array('includeClassname' => false, 'humanReadable' => true);
+		$_header = $this->CsvView->prepareHeaderFromExtract($_extract, $customHeaders, $options);
+
+		$this->response->download('RespuestaSalarios_'.date("Ymd").'-'.date("His").'.csv'); // <= setting the file name
+
+		$this->viewClass = 'CsvView.Csv';
+		$this->set(compact(
+			'data',
+			'_serialize',
+			'_header',
+			'_extract',
+			'_delimiter',
+			'_bom',
+			'_null'
+		));
 	}
 
 /**
