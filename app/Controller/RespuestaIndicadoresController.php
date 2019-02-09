e37a86e5e0e56034e8c0ff1a6ee543b6ad07f7af
SaBaLeRo22 <linkinpark.sabalero22@gmail.com>
2019-02-09 11:44:32 -0300

    Consultas

--- app/Controller/RespuestaIndicadoresController.php
+++ app/Controller/RespuestaIndicadoresController.php
@@ -23,10 +23,83 @@ class RespuestaIndicadoresController extends AppController {
  */
 	public function index() {
 		$this->RespuestaIndicadore->recursive = 0;
+		$this->paginate = array(
+			'limit' => '5',
+			'order' => array(
+				'Consulta.id' => 'asc', 'RespuestaIndicadore.id' => 'asc'
+			)
+		);
 		$this->set('respuestaIndicadores', $this->Paginator->paginate());
 	}
 
-/**
+	public function csv()
+	{
+		$this->RespuestaIndicadore->recursive = 0;
+		$data = $this->RespuestaIndicadore->find('all', array(
+			'Consulta.id' => 'asc', 'RespuestaIndicadore.id' => 'asc'
+		));
+
+		foreach ($data as $key => $rp) {
+			if(!empty($rp['RespuestaIndicadore']['valor'])){$data[$key]['RespuestaIndicadore']['valor'] = number_format ( $rp['RespuestaIndicadore']['valor'], '2', ',', '.');}
+			if(!empty($rp['RespuestaIndicadore']['minimo'])){$data[$key]['RespuestaIndicadore']['minimo'] = number_format ( $rp['RespuestaIndicadore']['minimo'], '2', ',', '.');}
+			if(!empty($rp['RespuestaIndicadore']['maximo'])){$data[$key]['RespuestaIndicadore']['maximo'] = number_format ( $rp['RespuestaIndicadore']['maximo'], '2', ',', '.');}
+			if(!empty($rp['RespuestaIndicadore']['menor'])){$data[$key]['RespuestaIndicadore']['menor'] = number_format ( $rp['RespuestaIndicadore']['menor'], '2', ',', '.');}
+			if(!empty($rp['RespuestaIndicadore']['mayor'])){$data[$key]['RespuestaIndicadore']['mayor'] = number_format ( $rp['RespuestaIndicadore']['mayor'], '2', ',', '.');}
+			if($rp['RespuestaIndicadore']['notificar']=='1'){$data[$key]['RespuestaIndicadore']['notificar']='SI';} else{$data[$key]['RespuestaIndicadore']['notificar']='NO';}
+			$data[$key]['RespuestaIndicadore']['estado'] = $rp['Estado']['nombre'];
+			$data[$key]['RespuestaIndicadore']['user_created'] = $this->Authake->getUsuario($rp['RespuestaIndicadore']['user_created']);
+			$data[$key]['RespuestaIndicadore']['user_modified'] = $this->Authake->getUsuario($rp['RespuestaIndicadore']['user_modified']);
+		}
+
+		$_delimiter = ';';
+		$_bom = true;
+		$_null = '';
+		$_serialize = 'data';
+
+		$_extract = array(
+			'RespuestaIndicadore.id',
+			'RespuestaIndicadore.consulta_id',
+			'RespuestaIndicadore.indicador',
+			'RespuestaIndicadore.valor',
+			'RespuestaIndicadore.alerta',
+			'RespuestaIndicadore.mensaje',
+			'RespuestaIndicadore.evento',
+			'RespuestaIndicadore.color',
+			'RespuestaIndicadore.menor',
+			'RespuestaIndicadore.minimo',
+			'RespuestaIndicadore.maximo',
+			'RespuestaIndicadore.mayor',
+			'RespuestaIndicadore.unidad',
+			'RespuestaIndicadore.notificar',
+			'RespuestaIndicadore.estado',
+			'RespuestaIndicadore.created',
+			'RespuestaIndicadore.modified',
+			'RespuestaIndicadore.user_created',
+			'RespuestaIndicadore.user_modified'
+		);
+
+		$excludePaths = array(); // Exclude all id fields
+		//$_extract = $this->CsvView->prepareExtractFromFindResults($data, $excludePaths);
+
+		$customHeaders = array();
+		$options = array('includeClassname' => false, 'humanReadable' => true);
+		$_header = $this->CsvView->prepareHeaderFromExtract($_extract, $customHeaders, $options);
+
+		$this->response->download('RespuestaIndicadores_'.date("Ymd").'-'.date("His").'.csv'); // <= setting the file name
+
+		$this->viewClass = 'CsvView.Csv';
+		$this->set(compact(
+			'data',
+			'_serialize',
+			'_header',
+			'_extract',
+			'_delimiter',
+			'_bom',
+			'_null'
+		));
+	}
+
+	/**
  * view method
  *
  * @throws NotFoundException
